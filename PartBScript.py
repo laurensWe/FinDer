3fa1be91746598393e3a04b1a19b0339080034b2
laurensWe <laurensweijs@hotmail.com>
2016-09-05 09:46:53 +0200

    initialization

---
+++ PartBScript.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 09:44:10 2016
+
+This program is for the assignment set of financial derivatives Part B, where a simulation of an portfolio of futures will be executed.
+
+@author: LARO
+"""
+
