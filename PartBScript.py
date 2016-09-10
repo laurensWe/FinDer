d2778317cdd0b952729927a3d9abdb6801c140c0
laurensWe <laurensweijs@hotmail.com>
2016-09-10 15:52:32 +0200

    Margin Account simulation added to the script

--- PartBScript.py
+++ PartBScript.py
@@ -22,13 +22,54 @@ import pandas as pd
 import numpy as np
 import scipy.stats as st
 
-
-
 xls = pd.ExcelFile('Crude Oil - Gold Data.xls')
 goldfut = xls.parse('Gold')
 cofut = xls.parse('Crude oil')
 
-cofut.plot(x='Date', y='Price')
+names = cofut.columns.tolist()
+names[names.index('Price')] = 'Crude Oil Future Price (in dollar)'
+cofut.columns = names
+
+cofut.plot(x='Date', y='Crude Oil Future Price (in dollar)')
 daily_vol = cofut.std()
 maintenance = st.norm.ppf(.99)*np.sqrt(3)*daily_vol
+
+cofut['Change'] =cofut['Price'].diff()
+
+#%%
+" This piece of code does the simulation of the margin account"
+
+#initialisation
+InitialAc = 98120
+MainteReq = 73590
+AmountMarginCalls = 0
+retrieve = 0
+Margin = [98120]
+Difference = [0]
+ContractSize = 1000
+
+initialIndex = 648
+
+for num in range(initialIndex,initialIndex + 105):  #648 is the index for 2008-09-02 and 754 is the index for 2009-02-02
+    DiffMargin = cofut['Change'][num+1] * ContractSize
+    Difference.append(DiffMargin)
+    tempMargin = Margin[num - initialIndex] + DiffMargin
+    
+    if tempMargin < MainteReq:
+        AmountMarginCalls = AmountMarginCalls + 1
+        Margin.append(InitialAc)   # if the maintenance requirement has been reached, the margin account should be filled to the initial margin value.
+    elif tempMargin > InitialAc:
+        Margin.append(InitialAc)   # If the Max (Initial Margin) has been reached, the money will be deposited on your account.
+        retrieve = retrieve + 1
+    else:
+        Margin.append(tempMargin)
+
+#%% Mooie plotjes van de Margin Account
+
+pd.DataFrame(Margin).plot()
+        
+    
+    
+
+
 
