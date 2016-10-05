13dd3bdbdce1f9b5cf0aa641657a6cbae65ec62d
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-10-05 22:01:41 +0200

    I'm thinking there is still a bug given the special case n=2, but this code already looks a lot cleaner

--- Assignemt2PartA-B.py
+++ Assignemt2PartA-B.py
@@ -14,41 +14,28 @@ class OptionPricer(object):
         self.setFinalPrice(K,self.tree,N,ls)
         self.fillTree(S,K,sigma,c,N,ls,self.tree)
         
-    def replicatingPortfolio(self,c,ls): 
-        def xyf(t,s,su,yu,xu,sd,yd,xd): 
-            cu,cd = (1+c,1-c) if ls=='long' or t+1<self.N else (1-c,1+c)
-            y = (xu-xd+yu*su*cu-yd*sd*cd)/(su*cu-sd*cd)
-            x =  xd+(yd-y)*sd*cd 
-            f = x + y*s
-            return {'x':x,'y':y,'f':f}
-        return xyf 
-        
     def setFinalPrice(self,K,tree,N,ls):
         for i in tree[N]:
             S = tree[N][i]['S']
-            S_= tree[N-1][i+1 if i<N else i-1]['S']
             if ls=='long':
-                tree[N][i].update({'f':max(0,S-K),'x':-float(S>K)*S_,'y':float(S>K)})
+                tree[N][i].update({'f':max(0,S-K),'x':-float(S>K)*K,'y':float(S>K)})
             else:
-                tree[N][i].update({'f':-max(0,S-K),'x':float(S>K)*S_,'y':-float(S>K)})
+                tree[N][i].update({'f':-max(0,S-K),'x':float(S>K)*K,'y':-float(S>K)})
      
     def initTree(self,S,sigma,N):
         price = lambda u,d: S*np.exp(sigma*np.sqrt(u/N)-sigma*np.sqrt(d/N))
         return {t:{t-2*i:{'S':price(t-i,i)} for i in range(t+1)} for t in range(N+1)}
       
     def fillTree(self,S,K,sigma,c,N,ls,tree):
-        # recur through the option tree
-        recur = self.replicatingPortfolio(c,ls)
         for t in range(N-1,-1,-1):
             for i in tree[t]:
-                tree[t][i].update(recur(t,tree[t][i]['S'],  #S
-                                     tree[t+1][i+1]['S'], #Su
-                                     tree[t+1][i+1]['y'], #yu
-                                     tree[t+1][i+1]['x'], #xu
-                                     tree[t+1][i-1]['S'], #Sd
-                                     tree[t+1][i-1]['y'], #yd
-                                     tree[t+1][i-1]['x']))#xd
-        return tree
+                self.recur(c,ls,{'0':tree[t][i],'u':tree[t+1][i+1],'d':tree[t+1][i-1]})
+
+    def recur(self,c,ls,t):
+        cu,cd = (1+c,1-c) if ls=='long' else (1-c,1+c)
+        t['0']['y'] = (t['u']['x']-t['d']['x']+t['u']['y']*t['u']['S']*cu-t['d']['y']*t['d']['S']*cd)/(t['u']['S']*cu-t['d']['S']*cd)
+        t['0']['x'] =  t['d']['x']+(t['d']['y']-t['0']['y'])*t['d']['S']*cd 
+        t['0']['f'] = t['0']['x'] + t['0']['y']*t['0']['S']
                  
     def toString(self):
         out = ''
@@ -67,11 +54,23 @@ class OptionPricer(object):
         return round(self.tree[0][0]['f'],2)
     
 if __name__=='__main__':
-    op = OptionPricer(S=S0,K=K,sigma=sigma,c=c,N=10,ls='short')
+    op = OptionPricer(S=S0,K=K,sigma=sigma,c=c,N=2,ls='short')
     s=op.toString()#(S0, K, sigma, c=c, N=1)
     option_price = op.price()
     n=1
     Along = OptionPricer(10,10,.2,.1,n,'long').price()
     Ashort= OptionPricer(10,10,.2,.1,n,'short').price()
     
+    s=OptionPricer(10,10,.2,.1,n,'short').toString()    
+    
+    from matplotlib import pyplot as plt
+    x = np.arange(1,5)
+    y = [OptionPricer(10,10,.2,.1,n,'short').price() for n in x]
+    z = [OptionPricer(10,10,.2,.1,n,'long').price() for n in x]
+    plt.xkcd()
+    plt.plot(x,y,c='r')
+    plt.plot(x,z,c='k')
+    plt.xlabel('no. of ticks per time unit')
+    plt.ylabel('bid-ask spread')
+    plt.title('long vs short on Euro call options')
      
